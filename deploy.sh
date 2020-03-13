31111c13680eca8776547482c70dccce5d8fb06e
Reinier G. Menendez <reinier.gonzalez@gmail.com>
2020-03-13 17:27:27 -0300

    update git commands on deploy script

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,6 @@ date &&
 git stash &&
 (git checkout hotfix || git checkout -b hotfix) &&
 git stash pop &&
-(git add * || true) &&
 (git commit -am "HOTFIXES TO $(date)" || true ) &&
 git checkout $1 && 
 git pull
