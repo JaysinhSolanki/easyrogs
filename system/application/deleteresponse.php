958cfcc76d71c895cd6104da6e79a788b82b3432
Hassan Ali <creativehassan59@gmail.com>
2020-04-01 02:44:11 +0500

    issue #53 and #55 solved

--- system/application/deleteresponse.php
+++ system/application/deleteresponse.php
@@ -2,6 +2,6 @@
 @session_start();
 require_once("adminsecurity.php");
 $response_id	=	$_POST['response_id'];
-$AdminDAO->displayquery=1;
+//$AdminDAO->displayquery=1;
 $AdminDAO->deleterows('responses',"id = :id",array("id"=>$response_id));
 $AdminDAO->deleterows('response_questions'," fkresponse_id = :fkresponse_id", array("fkresponse_id"=>$response_id)); 
