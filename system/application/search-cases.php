21ee1a287b40128269700b13721676ec9563fc43
Reinier G. Menendez <reinier.gonzalez@gmail.com>
2020-04-28 16:11:08 -0300

    Update Join Case search: only display cases when query is not empty (#172)
    Closes #171

--- system/application/search-cases.php
+++ system/application/search-cases.php
@@ -4,16 +4,19 @@
 
   $term = $_GET['term'];
   
-  // query
-  $cases = $casesModel->search($term);
+  $cases = [];
+  if (trim($term)) { // only search if query provided
+    // query
+    $cases = $casesModel->search($term);
 
-  // format response  
-  $response = [];
-  foreach($cases as &$case) {
-    $case = [
-      'id'   => $case['id'],
-      'text' => "$case[case_title] ($case[county_name] $case[case_number])"
-    ];
+    // format response  
+    $response = [];
+    foreach($cases as &$case) {
+      $case = [
+        'id'   => $case['id'],
+        'text' => "$case[case_title] ($case[county_name] $case[case_number])"
+      ];
+    }
   }
   
   HttpResponse::successPayload(["results" => $cases]);
