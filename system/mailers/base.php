cb3e9e011d7998345a30e988eb9993b3ba067748
Reinier G. Menendez <reinier.gonzalez@gmail.com>
2020-04-29 14:30:51 -0300

    Handle testing emails on prod (#184)

--- system/mailers/base.php
+++ system/mailers/base.php
@@ -1,21 +1,21 @@
 <?php
 
   class BaseMailer {
+    const TESTING_DOMAIN         = 'ezrogs.com';
+    const TESTING_DEV_RECIPIENTS = ['easyrogs@mailinator.com', 'easyrogs@gmail.com'];
+    const TESTING_PROD_RECIPIENT = 'easyrogs@gmail.com';
+    
     const FROM_EMAIL = 'service@easyrogs.com';
     const FROM_NAME  = 'EasyRogs Service';
 
-    static function sendEmail($to, $subject, $body, 
-      $fromName = self::FROM_NAME, 
-      $fromEmail = self::FROM_EMAIL, 
-      $attachments = [], $cc = [], $bcc = [] 
+    static function sendEmail(
+      $to, $subject, $body, 
+      $fromName = self::FROM_NAME, $fromEmail = self::FROM_EMAIL, 
+      $attachments = [], 
+      $cc = [], $bcc = [] 
     ) {
       global $logger;
 
-      // do not send real emails on dev
-      if ( $_ENV['APP_ENV'] != 'prod') {
-        $to = $cc = $bcc = ['easyrogs@mailinator.com', 'easyrogs@gmail.com'];
-      }
-      
       // validate params
       if (!($to && $subject && $body)) { 
         return $logger->error("Trying to send email with missing parts: To: $to, Subject: $subject, Body Length: ". strlen($body));
@@ -24,6 +24,20 @@
       // normalize params
       foreach([$to, $cc, $bcc, $attachments] as &$part) {
         $part = is_array($part) ? $part : [$part];
+      }
+
+      // do not send real emails on dev
+      if ($_ENV['APP_ENV'] != 'prod') {
+        $to = $cc = $bcc = self::TESTING_DEV_RECIPIENTS;
+      }
+      else { // handle testing emails on prod
+        foreach($to as &$email) {
+          $emailParts = explode('@', $email);
+          $domain = trim(strtolower($emailParts[1]));
+          if ($domain === self::TESTING_DOMAIN) {
+            $email = self::TESTING_PROD_RECIPIENT;
+          }
+        }
       }
 
       // initialize php mailer
