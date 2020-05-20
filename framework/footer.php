79b2bf3b13444a10178cd40c9974b1aaf4c46e42
Reinier G. Menendez <reinier.gonzalez@gmail.com>
2020-05-20 10:58:15 -0300

    Add Smartsupp code for live chat (#289)

--- framework/footer.php
+++ framework/footer.php
@@ -29,5 +29,18 @@ require_once("{$_SESSION['framework_path']}faq_modal.php");
   }
   window.setInterval( _ => checksession(), <?= $_ENV['APP_ENV'] == 'local' ? 3600000 : 10000 ?> );
 </script>
+
+<!-- Smartsupp Live Chat script -->
+<script type="text/javascript">
+	var _smartsupp = _smartsupp || {};
+	_smartsupp.key = 'ae242385584ca4d3fd78d74a04dbd806ef3957e0';
+	window.smartsupp||(function(d) {
+		var s,c,o=smartsupp=function(){ o._.push(arguments)};o._=[];
+		s=d.getElementsByTagName('script')[0];c=d.createElement('script');
+		c.type='text/javascript';c.charset='utf-8';c.async=true;
+		c.src='https://www.smartsuppchat.com/loader.js?';s.parentNode.insertBefore(c,s);
+	})(document);
+</script>
+
 </body>
 </html>
