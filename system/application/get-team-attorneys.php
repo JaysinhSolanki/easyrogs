a0cd17e234549c3b5df6fb16c77eb53bbdcc568b
Reinier G. Menendez <reinier.gonzalez@gmail.com>
2020-08-17 17:38:07 -0300

    Add side attorneys to lead counsel dropdown (#442)

--- system/application/get-team-attorneys.php
+++ system/application/get-team-attorneys.php
@@ -6,16 +6,29 @@
 
   if ( !$valid = $caseId ) { HttpResponse::malformed('Case ID is required.'); }
 
-  $attorneys    = $currentUser->getTeamAttorneys();
-  $userSide     = $sidesModel->getByUserAndCase($currentUser->id, $caseId);
-  $sideAttorney = $sidesModel->getPrimaryAttorney($userSide['id']);
+  $userSide        = $sidesModel->getByUserAndCase($currentUser->id, $caseId);
+  $attorneys       = $currentUser->getTeamAttorneys();
+  $sideUsers       = $sidesModel->getUsers($userSide['id']);
+  $primaryAttorney = $sidesModel->getPrimaryAttorney($userSide['id']);
   
+  $attorneys = $attorneys ? $attorneys : [];
+
   // add side attorney if not part of the team
-  if ( $sideAttorney ) {
-    if( !User::inCollection($sideAttorney, $attorneys) ) {
-      $attorneys[] = $sideAttorney;
+  if ($primaryAttorney) {
+    if( !User::inCollection($primaryAttorney, $attorneys) ) {
+      $attorneys[] = $primaryAttorney;
     }
   }
+
+  // also add side attorneys
+  if ($sideUsers) {
+    foreach($sideUsers as $user) {
+      if (User::isAttorney($user) && !User::inCollection($user, $attorneys)) {
+        $attorneys[] = $user;
+      }
+    }
+  }
+
   
   HttpResponse::successPayload(
     User::publishable($attorneys)
